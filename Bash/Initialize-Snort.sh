f46308956b95889d37cd4d41923002d51209093a
Skelter <130784780+skelter117@users.noreply.github.com>
2024-04-27 14:08:53 -0400

    Update Initialize-Snort.sh
    now appends the snort.conf and snort.lua network correctly

--- Bash/Initialize-Snort.sh
+++ Bash/Initialize-Snort.sh
@@ -10,17 +10,16 @@ sudo apt-get install snort -y
 
 read -p "Enter the network(s) to monitor (CIDR notation or multiple networks separated by commas): " networks
 
-snort_version=$(sudo snort --version | grep -oP 'Version \K\d')
+snort_version=$(sudo snort --version | grep -oP 'Version \K\d+\.\d+')
 
-if [[ $snort_version == 3.* ]]; then 
-    sudo sed -i "s|^HOME_NET.*|HOME_NET $networks|" /etc/snort/snort.lua
-elif [[ $snort_version == 2.* ]]; then 
-    sudo sed -i "s|^var HOME_NET.*|var HOME_NET $networks|" /etc/snort/snort.conf
+echo "Detected Snort version: $snort_version"
+
+if [[ $snort_version =~ 3\..* ]]; then 
+    echo "Updating HOME_NET for Snort 3.x"
+    sudo sed -i "s|^HOME_NET.*|HOME_NET = \"$networks\";|" /etc/snort/snort.lua
+elif [[ $snort_version =~ 2\..* ]]; then 
+    echo "Updating HOME_NET for Snort 2.x"
+    sudo sed -i "s|^var HOME_NET.*|var HOME_NET $networks;|" /etc/snort/snort.conf
 fi
 
 sudo cp /home/$user/Downloads/master/Wordlists/Cheatsheets/SnortRulesCheatsheet.txt /etc/snort/rules/local.rules
-
-
-#issue with updating the HOME_NET variable with $networks in the snort.lua file and snort.conf file
-
-#everything else works. working on it!
