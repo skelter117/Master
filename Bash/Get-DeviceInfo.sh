d087b534f6527a598cc7591f0eedaa31050d9651
Skelter <130784780+skelter117@users.noreply.github.com>
2024-04-29 09:09:39 -0400

    Update Get-DeviceInfo.sh
    updated if target device is windows

--- Bash/Get-DeviceInfo.sh
+++ Bash/Get-DeviceInfo.sh
@@ -1,40 +1,39 @@
 #!/bin/bash
 
-# Prompt the user to enter the hostname to check
-read -p "Enter the hostname you want to check on: " targetHostname
+read -p "Enter the IP address you want to check: " targetIP
 
-# Get the IP address associated with the hostname
-targetIP=$(host "$targetHostname" | grep 'has address' | awk '{print $4}')
-
-if [ -z "$targetIP" ]; then
-    echo "Hostname not found or does not have an IP address."
-else
-    # Get system information remotely
-    deviceModel=$(ssh "$targetIP" dmidecode -s system-product-name)
-    motherboardModel=$(ssh "$targetIP" dmidecode -s baseboard-product-name)
-    serialNumber=$(ssh "$targetIP" dmidecode -s system-serial-number || echo "Not Available")
-    ramCapacity=$(ssh "$targetIP" free -m | awk 'NR==2{print $2/1024}')
-    diskCapacity=$(ssh "$targetIP" df -h | awk '$NF=="/"{print $2}')
-    processorName=$(ssh "$targetIP" cat /proc/cpuinfo | grep "model name" | uniq | cut -d ":" -f 2 | sed 's/^ *//')
-    processorDescription=$(ssh "$targetIP" lscpu | grep "Model name" | awk -F ':' '{print $2}' | sed 's/^ *//')
-    macAddress=$(ssh "$targetIP" cat /sys/class/net/*/address | head -n 1)
-    windowsVersion=$(ssh "$targetIP" uname -a)
-    lastBootUpTime=$(ssh "$targetIP" who -b | awk '{print $3,$4}')
-    uptime=$(ssh "$targetIP" uptime | awk '{print $3,$4}')
-
-    # Display system information
-    echo "Device Model is: $deviceModel"
-    echo "Motherboard Model is: $motherboardModel"
-    echo "Serial Number is: $serialNumber"
-    echo "Total RAM Capacity (GB): $ramCapacity"
-    echo "HDD/SSD Size (C: drive) (GB): $diskCapacity"
-    echo "Processor Name is: $processorName"
-    echo "Processor Description: $processorDescription"
-    echo "MAC Address: $macAddress"
-    echo "Windows Version is: $windowsVersion"
-    echo "Last Boot Up Time: $lastBootUpTime"
-    echo "Uptime: $uptime"
+# Validating IP address format
+if ! [[ $targetIP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "Invalid IP address format."
+    exit 1
 fi
+
+# Retrieving system information
+deviceModel=$(sudo dmidecode -t system | awk -F: '/Product Name/ {print $2}' | tr -d '[:space:]')
+motherboardModel=$(sudo dmidecode -t baseboard | awk -F: '/Product Name/ {print $2}' | tr -d '[:space:]')
+serialNumber=$(sudo dmidecode -t system | awk -F: '/Serial Number/ {print $2}' | tr -d '[:space:]')
+ramCapacity=$(grep MemTotal /proc/meminfo | awk '{print $2 / 1024 / 1024}')
+diskCapacity=$(df -h | awk '$6 == "/" {print $2}')
+usedDiskSpace=$(df -h | awk '$6 == "/" {print $3}')
+processorName=$(grep "model name" /proc/cpuinfo | uniq | awk -F: '{print $2}' | tr -d '[:space:]')
+processorDescription=$(lscpu | grep "Model name" | awk -F: '{print $2}' | tr -d '[:space:]')
+lastBootUpTime=$(uptime -s)
+uptime=$(uptime -p)
+macAddress=$(ip link | awk '$1 == "link/ether" {print $2; exit}')
+
+# Printing system information
+echo "Device Model is: $deviceModel"
+echo "Motherboard Model is: $motherboardModel"
+echo "Serial Number is: ${serialNumber:-Not Available}"
+echo "Total RAM Capacity (GB): $ramCapacity"
+echo "HDD/SSD Size (C: drive) (GB): $diskCapacity"
+echo "Used Disk Space (C: drive) (GB): $usedDiskSpace"
+echo "Processor Name is: $processorName"
+echo "Processor Description: $processorDescription"
+echo "Last Boot Up Time: $lastBootUpTime"
+echo "Uptime: $uptime"
+echo "MAC Address: ${macAddress:-Not Available}"
+
 
 #if the device target is linux
 #!/bin/bash
