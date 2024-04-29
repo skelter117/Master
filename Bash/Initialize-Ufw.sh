46d720327ee0aca18dfe2a3b0878d50631da2f49
Skelter <130784780+skelter117@users.noreply.github.com>
2024-04-29 14:11:23 -0400

    Update Initialize-Ufw.sh
    added prompt to specify if the admin wants to allow tcp 22 from a specific ip address

--- Bash/Initialize-Ufw.sh
+++ Bash/Initialize-Ufw.sh
@@ -19,7 +19,15 @@ done
 echo "Adding explicit deny rule..."
 sudo ufw default deny incoming
 
-read -p "Do you want to enable the firewall? (y/n) " decision
+read -p "Do you want to allow ssh from a specific IP? (yes/no): " response
+if [[ $response == "yes" ]]; then
+  read -p "Which IP do you want to allow? " IP
+  sudo ufw allow from $IP to any proto tcp port 22
+else
+  echo "Okay, not allowing SSH from a specific IP."
+fi
+
+read -p "Do you want to enable the firewall? (y/n): " decision
 if [[ "$decision" =~ ^[Yy]$ ]]; then
   sudo ufw enable && echo "Firewall enabled." || { echo "Failed to enable firewall."; exit 1; }
 else
@@ -27,3 +35,4 @@ else
 fi
 
 exit 0
+
