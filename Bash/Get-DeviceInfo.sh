39fa33c12d583b40926696a57ca37fde856bf779
Skelter <130784780+skelter117@users.noreply.github.com>
2024-04-29 08:55:09 -0400

    Update Get-DeviceInfo.sh
    updated if target is linux

--- Bash/Get-DeviceInfo.sh
+++ Bash/Get-DeviceInfo.sh
@@ -35,3 +35,37 @@ else
     echo "Last Boot Up Time: $lastBootUpTime"
     echo "Uptime: $uptime"
 fi
+
+#if the device target is linux
+#!/bin/bash
+
+read -p "Enter the hostname or IP address you want to check on: " targetHostname
+
+# Resolve the IP address of the target
+targetIP=$(getent ahosts "$targetHostname" | awk '/^([0-9]{1,3}\.){3}[0-9]{1,3}/{print $1; exit}')
+
+if [ -z "$targetIP" ]; then
+    echo "Hostname not found or does not have an IP address."
+else
+    # Retrieve system information
+    model=$(sudo dmidecode -s system-product-name)
+    motherboard=$(sudo dmidecode -s baseboard-product-name)
+    serial=$(sudo dmidecode -s system-serial-number)
+    ram_capacity=$(free -h | awk '/^Mem:/{print $2}')
+    disk_capacity=$(df -h | awk '/^\/dev\/[a-z]/ && $NF=="/"{print $2}')
+    used_disk_space=$(df -h | awk '/^\/dev\/[a-z]/ && $NF=="/"{print $3}')
+    processor=$(cat /proc/cpuinfo | grep "model name" | uniq | cut -d ":" -f 2 | sed 's/^[ \t]*//')
+    last_bootup=$(uptime -s)
+    current_user=$(who | awk 'NR==1{print $1}')
+
+    echo "Target IP: $targetIP"
+    echo "Device Model: $model"
+    echo "Motherboard Model: $motherboard"
+    echo "Serial Number: ${serial:-Not Available}"
+    echo "Total RAM Capacity: $ram_capacity"
+    echo "Disk Capacity: $disk_capacity"
+    echo "Used Disk Space: $used_disk_space"
+    echo "Processor Name: $processor"
+    echo "Last Bootup Time: $last_bootup"
+    echo "Currently logged-in user: $current_user"
+fi
