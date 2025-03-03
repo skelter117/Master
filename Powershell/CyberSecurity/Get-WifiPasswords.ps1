06a1343f599e2178845ff22609a464aae31323bf
Skelter <130784780+skelter117@users.noreply.github.com>
2025-03-03 09:35:00 -0500

    Update Get-WifiPasswords.ps1
    updated powershell script

--- Powershell/CyberSecurity/Get-WifiPasswords.ps1
+++ Powershell/CyberSecurity/Get-WifiPasswords.ps1
@@ -1,1 +1,2 @@
 #Print out wifi passwords stored on local computer
+netsh wlan show profiles | ForEach-Object { if ($_ -like "*ll User Profile*") { $ssid = $_.Split(":")[1].Trim(); netsh wlan show profile name="$ssid" key=clear | ForEach-Object { if ($_ -like "*Key Content*") { "$ssid : " + $_.Split(":")[1].Trim() } } } }
